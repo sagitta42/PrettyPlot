63cb24abc89a80039f1a2f226cff0629c03ec478
sagitta42 <sssagitta@gmail.com>
2021-02-16 19:32:42 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prettyplot',
+    version='0.0',
+    author='Mariia Redchuk',
+    author_email='mariia.redchuk@gmail.com',
+    description='Python class for quick and pretty plotting',
+    long_description='',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib'
+#        'numpy',
+#        'pandas',
+        # 'fcutils @ https://github.com/legend-exp/pyfcutils.git#egg=1.0.0'
+    ],
+#    cmdclass=dict(build_ext=CMakeBuild, build_py=PygamaBuild, develop=PygamaDev),
+    zip_safe=False,
+)
