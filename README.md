682993a59657fc9605940fd11ebbdda953c67381
sagitta42 <sssagitta@gmail.com>
2022-02-15 21:46:27 +0100

    troubleshooting

---
+++ README.md
@@ -0,0 +1,5 @@
+# prettyplot
+
+```console
+sudo -H python3 -m pip install git+https://github.com/sagitta42/prettyplot
+```
--- setup.py
+++ setup.py
@@ -1,40 +1,20 @@
-from setuptools import setup
-import os
-import sys
-
-_here = os.path.abspath(os.path.dirname(__file__))
-
-if sys.version_info[0] < 3:
-    with open(os.path.join(_here, 'README.rst')) as f:
-        long_description = f.read()
-else:
-    with open(os.path.join(_here, 'README.rst'), encoding='utf-8') as f:
-        long_description = f.read()
-
-version = {}
-with open(os.path.join(_here, 'prettyplot', 'version.py')) as f:
-    exec(f.read(), version)
+from setuptools import setup, find_packages
 
 setup(
     name='prettyplot',
-    version=version['__version__'],
-    description=('Framework for quick and pretty plotting.'),
-    long_description=long_description,
+    version='0.0',
     author='Mariia Redchuk',
     author_email='mariia.redchuk@gmail.com',
     url='https://github.com/sagitta42/prettyplot',
-    license='MPL-2.0',
-    packages=['prettyplot'],
-#   yes dependencies in this example
-   install_requires=[
-       'matplotlib'#==3.3.2',
-   ],
-#   no scripts in this example
-#   scripts=['bin/a-script'],
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6'],
-    )
+    description='Python class for quick and pretty plotting',
+    long_description='',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib'
+#        'numpy',
+#        'pandas',
+        # 'fcutils @ https://github.com/legend-exp/pyfcutils.git#egg=1.0.0'
+    ],
+#    cmdclass=dict(build_ext=CMakeBuild, build_py=PygamaBuild, develop=PygamaDev),
+    zip_safe=False,
+)
